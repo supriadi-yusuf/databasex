1f562cb3af284672c0dd334f3f0d4c4154eb20cb
supriadi-yusuf <supriadi96@gmail.com>
2020-12-05 22:01:57 +0700

    add method CreateModel on interface IModelFactory

--- model_factory.go
+++ model_factory.go
@@ -3,12 +3,17 @@ package databasex
 // IModelFactory is interface defining how to create model.
 type IModelFactory interface {
 	NewModel(tableName string, data interface{}) (model IWriteableModel)
+	CreateModel(tableName string, data interface{}) (model IWriteableModel)
 }
 
 type modelFactory struct{}
 
 func (mdl *modelFactory) NewModel(tableName string, data interface{}) (model IWriteableModel) {
 	return NewSimpleModel(tableName, data)
+}
+
+func (mdl *modelFactory) CreateModel(tableName string, data interface{}) (model IWriteableModel) {
+	return mdl.NewModel(tableName, data)
 }
 
 // CreateModelFactory is function to create model factory
