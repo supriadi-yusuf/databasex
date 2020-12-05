6d2b0f0918e16c04fd7c03a73321ece886d6bb65
supriadi-yusuf <supriadi96@gmail.com>
2020-12-05 21:48:05 +0700

    add method CreateSQLOperation on interface ISqlFactory

--- sql_factory.go
+++ sql_factory.go
@@ -3,12 +3,17 @@ package databasex
 // ISqlFactory is interface for creating factory of object implementing ISqlOperation
 type ISqlFactory interface {
 	NewSQLOperation(db IDatabase) ISqlOperation
+	CreateSQLOperation(db IDatabase) ISqlOperation
 }
 
 type sqlFactory struct{}
 
 func (factory *sqlFactory) NewSQLOperation(db IDatabase) ISqlOperation {
 	return NewSimpleSQL(db)
+}
+
+func (factory *sqlFactory) CreateSQLOperation(db IDatabase) ISqlOperation {
+	return factory.NewSQLOperation(db)
 }
 
 // CreateSQLFactory is function to create factory of object implementing ISqlOperation
